8978664d8f9a6cf9b96e5b783181e39ebf0eedf7
demikandr <dimanonim@gmail.com>
2015-09-15 15:19:16 +0300

    Create ReverseInput.java

---
+++ src/ru/fizteh/fivt/students/DShchelchkov/Reverse/ReverseInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ReverseInput {
+	
+	public static void main (String args[]) {
+		
+		 for(int i=0; i < args.length; i++){
+			System.out.println(args[args.length-1-i]);
+		}
+		
+	}
+}
